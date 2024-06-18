85729ba46d639d5edd622aca038a3de1b24503a8
Filip Jirsák <filip@jirsak.org>
2024-06-18 20:39:57 +0200

    Typ pro ISBN v DB jako varchar.
    Pokud se použije CHAR, doplňuje databáze za kratší ISBN mezery a pak to neprojde při aktualizaci backendovou validací.

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -4,7 +4,7 @@ CREATE TABLE kniha
     nazev      VARCHAR(255) NOT NULL,
     autor      VARCHAR(255) NOT NULL,
     rok_vydani SMALLINT,
-    isbn       CHAR(13) CHECK REGEXP_LIKE(isbn, '[0-9]{13}|[0-9]{9}[0-9X]', ''),
+    isbn       VARCHAR(13) CHECK REGEXP_LIKE(isbn, '[0-9]{13}|[0-9]{9}[0-9X]', ''),
     stornovano BOOLEAN      NOT NULL DEFAULT FALSE,
     version    INTEGER      NOT NULL
 );
